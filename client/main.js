00989fb16012a442ebe8e86b18f21413cdbbbf82
Achi06 <achala.singh94@gmail.com>
2018-05-16 20:34:11 +1000

    UI: Move between Templates

--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ import './main.html';
 import '../imports/ui/body.html';
 import '../imports/ui/admin.html';
 import '../imports/ui/admin.css';
+import '../imports/api/admin.js';
 
 Router.route('/', function(){
     this.render('start')
@@ -12,23 +13,4 @@ Router.route('/', function(){
 
 Router.route('/admin', function(){
     this.render('admin')
-});
-
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  });
---
+++ imports/api/admin.js
@@ -0,0 +1,7 @@
+Router.route('/menu-1', function(){
+    this.render('menu-1')
+});
+
+Router.route('/menu-2', function(){
+    this.render('menu-2')
+});
