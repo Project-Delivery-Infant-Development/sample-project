df8aaaaa096785f069fc3dd3004a53c975d7289e
Achi06 <achala.singh94@gmail.com>
2018-05-16 23:26:04 +1000

    ADMIN: Lga forms and Lists

--- imports/api/admin.js
+++ imports/api/admin.js
@@ -5,3 +5,11 @@ Router.route('/menu-1', function(){
 Router.route('/menu-2', function(){
     this.render('menu-2')
 });
+
+Router.route('/view-lga', function(){
+    this.render('view-lga')
+});
+
+Router.route('/add-lga', function(){
+    this.render('add-lga')
+});
