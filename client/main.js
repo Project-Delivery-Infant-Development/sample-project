929a68361f005a3631ace3500d815866deb6eae4
Achi06 <achala.singh94@gmail.com>
2018-05-22 20:54:34 +1000

    ADD UI: Admin, Facilitator, LGA

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import '../imports/ui/body.html';
 import '../imports/ui/admin.html';
+import '../imports/ui/lga.html';
+import '../imports/ui/faci.html';
 import '../imports/ui/admin.css';
 import '../imports/api/admin.js';
 
@@ -15,10 +17,10 @@ Router.route('/admin', function(){
     this.render('admin')
   });
 
-  Template.viewlga.helpers({
-    tasks: [
-      { text: 'This is task 1' },
-      { text: 'This is task 2' },
-      { text: 'This is task 3' },
-    ],
+Router.route('/lga', function(){
+      this.render('lga')
+  });
+
+Router.route('/faci', function(){
+          this.render('faci')
   });
