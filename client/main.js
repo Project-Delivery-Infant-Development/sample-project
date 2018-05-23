be1c3976780945d590cd9785045081072f44a30a
Yashneet Pal Singh <yashn@deakin.edu.au>
2018-05-23 23:06:17 +1000

    ADD: login & Registeration

--- client/main.js
+++ client/main.js
@@ -1,5 +1,9 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import Tabular from 'meteor/aldeed:tabular';
+import moment from 'moment';
+import { Meteor } from 'meteor/meteor';
+import { Books } from './collections/Books';
 
 import './main.html';
 import '../imports/ui/body.html';
@@ -25,3 +29,28 @@ Router.route('/lga', function(){
 Router.route('/faci', function(){
           this.render('faci')
   });
+
+  // new Tabular.Table({
+  //   name: "Books",
+  //   collection: Books,
+  //   columns: [
+  //     {data: "title", title: "Title"},
+  //     {data: "author", title: "Author"},
+  //     {data: "copies", title: "Copies Available"},
+  //     {
+  //       data: "lastCheckedOut",
+  //       title: "Last Checkout",
+  //       render: function (val, type, doc) {
+  //         if (val instanceof Date) {
+  //           return moment(val).calendar();
+  //         } else {
+  //           return "Never";
+  //         }
+  //       }
+  //     },
+  //     {data: "summary", title: "Summary"},
+  //     {
+  //       tmpl: Meteor.isClient && Template.bookCheckOutCell
+  //     }
+  //   ]
+  // });
